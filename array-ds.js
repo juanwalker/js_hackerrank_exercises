bcaedc4a221d872bdca07a256da8263d7ebc09a9
Juan Pablo <juanwalker@gmail.com>
2024-03-16 10:00:40 -0500

    Array-ds solution

---
+++ array-ds.js
@@ -0,0 +1,13 @@
+/*
+ * Complete the 'reverseArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+var array = [1,4,3,2]
+console.log("input :" + array)
+function reverseArray(a) {
+    // Write your code here
+    return a.reverse()
+}
+console.log("output :" + reverseArray(array))
